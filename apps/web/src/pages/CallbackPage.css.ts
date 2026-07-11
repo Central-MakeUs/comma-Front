1d829e8a7bf000a408a985118c4a23647a2a489e
hozorica <67588757+choihooo@users.noreply.github.com>
2026-07-11 16:11:50 +0900

    feat: 로그인 API 호출 개선 (#86)
    * feat: 로그인 API 호출 개선
    
    * refactor: 인증 API 인스턴스 분리
    
    * refactor: api 응답 타입 공통화

---
+++ apps/web/src/pages/CallbackPage.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  backgroundImage: 'url("/images/onboardingBackground_blur.png")',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  width: '100vw',
+  height: '100vh'
+});
